36c4ccc14da64f63a31d9de6c9cbd08a55967280
Rajesh Pillai <rajesh@algorisys.com>
2021-01-11 17:17:20 +0530

    New ecommerce repo created

---
+++ components/login-screen.js
@@ -0,0 +1,17 @@
+
+const LoginScreen = {
+  render: function() {
+    return `
+    <form id="login-form">
+      <input type="text" placeholder="username" id="username"/>
+      <input type="password" placeholder="password" id="password"/>
+      <button type="submt">Submit</button>
+      <a href="#">Forgot Password</a>
+      <a href="#">New User</a>
+     </form>
+   `;
+  }
+}
+
+
+export default LoginScreen;
---
+++ index.js
@@ -0,0 +1,4 @@
+import LoginScreen from './components/login-screen.js';
+
+const rootEle = document.querySelector("#root");
+rootEle.innerHTML = LoginScreen.render();
