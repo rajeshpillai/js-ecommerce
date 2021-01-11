52a7e5556a622cde78e112ec3bfa89ed0c0a36c1
Rajesh Pillai <rajesh@algorisys.com>
2021-01-11 18:06:29 +0530

    Notes

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ import LoginScreen from './components/login-screen.js';
 
 const rootEle = document.querySelector("#root");
 rootEle.innerHTML = LoginScreen.render();
+
+// Check if after_render method is defined
+//    If yes, invoke after_render()
 if (LoginScreen.after_render) {
   LoginScreen.after_render();
 }
