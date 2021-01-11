1bb6adcfb10b70a8700b7ebcffa7fec3565edbd1
Rajesh Pillai <rajesh@algorisys.com>
2021-01-11 17:58:36 +0530

    Login: event handler better way

--- components/login-screen.js
+++ components/login-screen.js
@@ -1,9 +1,16 @@
 
 
 const LoginScreen = {
+  after_render: function() {
+    document.querySelector("#login-form")
+      .addEventListener("submit", function(e) {
+        e.preventDefault();
+        alert('Click...');
+      });
+  },
   render: function() {
     return `
-    <form id="login-form" onsubmit='onSubmit()'>
+    <form id="login-form">
       <input type="text" placeholder="username" id="username"/>
       <input type="password" placeholder="password" id="password"/>
       <button type="submt">Submit</button>
--- index.js
+++ index.js
@@ -2,3 +2,6 @@ import LoginScreen from './components/login-screen.js';
 
 const rootEle = document.querySelector("#root");
 rootEle.innerHTML = LoginScreen.render();
+if (LoginScreen.after_render) {
+  LoginScreen.after_render();
+}
