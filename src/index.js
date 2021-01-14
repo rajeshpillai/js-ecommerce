66401325d628f2b804aeef166eaebe2189655fd4
Rajesh Pillai <rajesh@algorisys.com>
2021-01-14 16:28:45 +0530

    Added hash router and 404screen

--- src/index.js
+++ src/index.js
@@ -1,16 +1,41 @@
 import LoginScreen from './components/login-screen.js';
 import HomeScreen from './components/home-screen';
+import Error404Screen from './components/error404-screen';
 
 import './styles/site.css';
 
 const rootEle = document.querySelector("#root");
 
-const router = async () => {
-  rootEle.innerHTML = await HomeScreen.render();
-  // Check if after_render method is defined
-  //    If yes, invoke after_render()
-  if (HomeScreen.after_render) {
-    HomeScreen.after_render();
+const routes = {
+  '/': HomeScreen,
+  '/signin': LoginScreen,
+}
+
+const parseRequestUrl = () => {
+  const url = document.location.hash.toLowerCase();
+  const request = url.split('/');
+  return {
+    resource: request[1],
+    id: request[2],
+    verb: request[3]
   }
 }
-router();
+
+const router = async () => {
+  const request = parseRequestUrl();
+  const parseUrl = (request.resource? `/${request.resource}` : '/') +
+            (request.id ? '/:id' : '') +
+            (request.verb ? `/${request.verb}` : '');
+   //alert(parseUrl);         
+
+  const screen = routes[parseUrl] ? routes[parseUrl]: Error404Screen;
+  rootEle.innerHTML = await screen.render();
+  if (screen.after_render) {
+    screen.after_render();
+  }
+}
+window.addEventListener("load", router);
+window.addEventListener("hashchange", router);
+
+
+
